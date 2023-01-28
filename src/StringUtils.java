87f9639ed22c061d5c9ba7388bae976570c580e3
freddieduffield <fwhduffield@gmail.com>
2023-01-28 21:16:33 +0000

    4: betterEntry / Two element predicate

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -5,11 +5,15 @@ public class StringUtils {
         return lambda.test(s1, s2) ? s1 : s2;
     }
 
+    static String betterEntry(String s1, String s2, TwoElementPredicate lambda) {
+        return lambda.test(s1, s2) ? s1 : s2;
+    }
+
     public static void main(String[] args) {
-        TwoStringPredicate betterTest1 = (s1, s2) -> s1.length() > s2.length();
-        String best = betterString("Fred", "Sarai", betterTest1);
+        TwoElementPredicate<String> betterTest1 = (s1, s2) -> s1.length() > s2.length();
+        String best = betterEntry("Fred", "Sarai", betterTest1);
         System.out.println(best);
-        best = betterString("Fred", "Sarai", (s1, s2) -> true);
+        best = betterEntry("Fred", "Sarai", (s1, s2) -> true);
         System.out.println(best);
     }
 }
---
+++ src/TwoElementPredicate.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface TwoElementPredicate<T> {
+    boolean test(T t1, T t2);
+}
